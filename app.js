e877d03ed37a03abbdbec247c623a10ec12222ec
edmundo096 <edmundo.geg@hotmail.com>
2016-02-26 00:12:14 -0600

    Clean main and routes, and redefine results routes.

--- app.js
+++ app.js
@@ -9,39 +9,34 @@ var app = express();
 app.set('viewsDir', __dirname + '/public'); // Later, move all and change to /views
 
 
-
+// Set middleware.
 app.use(express.static('public'));
 
 
 // Routing
-var routeRoot = require('./routes/root');
-var resultRouter = require('./routes/results');
 
+// Root.
+var routeRoot = require('./routes/root');
+app.get('/', routeRoot.root);
+app.get('/results', routeRoot.root);
+
+// Root's Results.
+var resultRouter = require('./routes/results');
+app.use('/results', resultRouter);
+
+// Todo: rest of routes... (dont forget to change the relative paths to start/be relative to root. E.g. index.html to /, or usuario.html to /user)
+// Rest of routes.
 var routeLogin = require('./routes/login');
 var routeRegister = require('./routes/register');
 var userRouter = require('./routes/user');
 var routeCart = require('./routes/cart');
-
-// Root.
 app.get('/carrito', routeCart.root);
 app.get('/login', routeLogin.root);
 app.use('/usuario', userRouter);
 app.get('/registro', routeRegister.root);
 
 
-app.get('/', routeRoot.root);
-app.get('/results', routeRoot.root);
-
-// Root's Results.
-app.use('/results', resultRouter);
-//app.get('/results/:category', routeResult.viewCategory);
-//app.get('/results/:category/:restaurant', routeResult.viewRestaurant);
-// Idea app.get('/results/:category/:restaurant/details', routeResult.viewRestaurantDetails);
-
-// Todo: rest of routes... (dont forget to change the relative paths to start/be relative to root. E.g. index.html to /, or usuario.html to /user)
-
-
-
+// Config and launch.
 var port = process.env.PORT || process.argv[2] || 80;
 
 app.listen(port, function() {
--- routes/results.js
+++ routes/results.js
@@ -9,22 +9,25 @@ var router = express.Router();
 // Todo: Log any param not found on DB.. (it maybe a broken link or a misstyped element) or catch 404 errors
 // Todo: get the results json from a specific url with params (/api/result?cat&rest), with the same custom url (/:cat..) but with getType=json, or with custom url plus api param (/:cat../api or /:cat..?api)
 
-// Refers, for example, to: '/results/'
+// Refers, for example, to: '/results/'.
 router.route('/')
   .get(function(req, res) {
     res.sendFile(req.app.get('viewsDir') + '/index.html');
   });
 
-router.route('/:category')
+router.route('/category/:category')
   .get(function(req, res) {
     console.log('Category: ', req.params.category);
     res.sendFile(req.app.get('viewsDir') + '/index.html'); // Todo: see if there is a wey to reuse the root path response (wildcard, app.use, middleware?)
   });
 
-router.route('/:category/:restaurant')
+router.route('/category/:category/restaurant/:restaurant')
   .get(function(req, res) {
     console.log('Category: ', req.params.category, 'Restaurant: ', req.params.restaurant);
     res.sendFile(req.app.get('viewsDir') + '/index.html');
   });
+
+// Idea app.get('/results/:category/:restaurant/details', routeResult.viewRestaurantDetails);
+
 
 module.exports = router;
