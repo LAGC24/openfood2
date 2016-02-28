7e64310c5a2bd13b5cfa1753f44665fcd69ce46a
edmundo096 <edmundo.geg@hotmail.com>
2016-02-28 01:51:09 -0600

    Clean node main code.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-
+// Set DB.
 var mongoUtil = require('./utils/mongoUtil');
 mongoUtil.connect();
 
@@ -18,22 +18,22 @@ app.use(express.static('public'));
 
 
 // Routing
+var routeRoot       = require('./routes/root');
+var resultRouter    = require('./routes/results');
+var routeLogin      = require('./routes/login');
+var routeRegister   = require('./routes/register');
+var userRouter      = require('./routes/user');
+var routeCart       = require('./routes/cart');
 
 // Root.
-var routeRoot = require('./routes/root');
 app.get('/', routeRoot.root);
 app.get('/results', routeRoot.root);
 
 // Root's Results.
-var resultRouter = require('./routes/results');
 app.use('/results', resultRouter);
 
 // Todo: rest of routes... (dont forget to change the relative paths to start/be relative to root. E.g. index.html to /, or usuario.html to /user)
 // Rest of routes.
-var routeLogin = require('./routes/login');
-var routeRegister = require('./routes/register');
-var userRouter = require('./routes/user');
-var routeCart = require('./routes/cart');
 app.get('/carrito', routeCart.root);
 app.get('/login', routeLogin.root);
 app.use('/usuario', userRouter);
@@ -44,5 +44,5 @@ app.get('/registro', routeRegister.root);
 var port = process.env.PORT || process.argv[2] || 80;
 
 app.listen(port, function() {
-  console.log('Running Express, running on port ', port)
+  console.log('Running Express, running on port', port)
 });
